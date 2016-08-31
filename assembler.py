3cb14d360c40e50e1ef1ee687914f0c0ed96cc2f
aleksei-udalov <aleksei.udalov.01@gmail.com>
2016-08-31 20:24:18 +0300

    some refactoring

--- assembler.py
+++ assembler.py
@@ -1,71 +1,98 @@
 #!/usr/bin/python3
-
 """Assembles bytecode file.
 
 Source must be specified in first argument.
 Assembled file saves in source directory with .zl extension"""
 from opcodes import opcodes
-import os, sys, struct
+import os, sys, struct, platform
 
-# format for struct.pack function. Change '<' to '>' to change byte order
-fmt = '<i'
 command_length = 4
 
-def die(message, linenum = None):
-    """Print error message and exit with status 1"""
-    print('Error: ' + message + \
-          (' at line ' + str(linenum + 1) if linenum != None else '') + \
-          ', exiting')
-    sys.exit(1)
+class Bytecode:
+    """Describes bytecode"""
+    # format for struct.pack function. Change '<' to '>' to change byte order
+    fmt = '<i'
+    def __init__(self):
+        self.bytecode = bytearray()
+        
+    def append(self, value):
+        self.bytecode += struct.pack(self.fmt, value)
+        
+    def save(self, source):
+        filename = source + '.zl'
+        with open(filename, 'bw') as output_file:
+            output_file.write(self.bytecode)
+        print('Bytecode size:', len(self))
+        return filename
+    	
+    def __len__(self):
+        return len(self.bytecode)
 
-if len(sys.argv) < 2:
-    die('not specified input file, please specify it in first argument')
-
-output = bytearray()
+    def __setitem__(self, addr, value):
+    	self.bytecode[addr : addr + command_length] = struct.pack(self.fmt, value)
+        
 
 class Label:
     """Describes one label"""
-    def __init__(self):
+    def __init__(self, name):
         self.transitions = []
-        self.address = -1
+        self.address = None
+        self.name = name
         
-labels = {}
+    def set_transitions(self, bytecode):
+        if self.address == None:
+            die('needs ' + self.name + ' label')
+        for transition in self.transitions:
+            bytecode[transition] = self.address
 
 class Var():
     """Describes one global variable"""
     def __init__(self, size):
         self.links = []
         self.size = size
+    def set_links(self, bytecode):
+        for link in self.links:
+            bytecode[link] = len(bytecode) // command_length
+    def allocate_space(self, bytecode):
+        for x in range(self.size):
+            bytecode.append(0)
+        
+def die(message, linenum = None):
+    """Print error message and exit with status 1"""
+    if linenum == None:
+        print('Error: ' + message + ', exiting')
+    else:
+        print('Error: ' + message + ' at line ' + str(linenum + 1) + ', exiting')    
+    sys.exit(1)
 
-vars_dict = {}
-
+if len(sys.argv) < 2:
+    die('not specified input file, please specify it in first argument')
+bytecode = Bytecode()
+labels = {}
+vars = {}
+source = sys.argv[1]
 print('Assembling...')
-with open(sys.argv[1]) as source_file:
+with open(source) as source_file:
     for linenum, line in enumerate(source_file):
         line = line.strip()
-        if not line:
-            continue
-        if line[0] in ('#', '/'):   # comment
+        if not line or line[0] in ('#', '/'):   # blank line or comment
             continue
         command, *arguments = line.split()
         command = command.upper()
-        # print('command = ' + command + ' arguments[0] = ' + \
-        #       (arguments[0] if len(arguments) > 0 else  ''))
         if command[0] == ':':   # label
             label_name = command[1:]
             if label_name not in labels:
-                labels[label_name] = Label()
+                labels[label_name] = Label(label_name)
             # it's important to remember that addressing
             # in stack-machine performed by dwords, not by bytes.
             # the smallest element that can be addressed is dword (integer).
             # it's the reason why in next line offset divides by command len.
-            labels[label_name].address = len(output) // command_length
+            labels[label_name].address = len(bytecode) // command_length
             continue
         if command == 'VAR':    # global variable declaration
             if len(arguments) < 1:
                 die('need var name', linenum)
             var = arguments[0]
-            size = 1
             if len(arguments) > 1:  # array
                 try:
                     size = int(arguments[1])
@@ -73,13 +100,15 @@ with open(sys.argv[1]) as source_file:
                     die('bad array ' + var + ' size: must be int', linenum)
                 if size < 1:
                     die(var + 'negative or zero size of array', linenum)
-            vars_dict[var] = Var(size)
+            else: # just a variable
+            	size = 1
+            vars[var] = Var(size)
             continue
         if command not in opcodes:
             die('Unknown command ' + command, linenum)
         # adding opcode to output
         # this line executes for all vm commands
-        output += struct.pack(fmt, opcodes.index(command))
+        bytecode.append(opcodes.index(command))
         # opcode added to output and now time to do some additional steps
         if command in ('JMP', 'JE', 'JL', 'JG', 'JNE', 'JLE', 'JGE', 'CALL'):
             if not arguments:
@@ -88,76 +117,60 @@ with open(sys.argv[1]) as source_file:
             if label_name[0] == ':':
                 label_name = label_name[1:]
             if label_name not in labels:
-                labels[label_name] = Label()
-            labels[label_name].transitions.append(len(output))
-            output += struct.pack(fmt, 0) # temporarily add 0 instead of addr
+                labels[label_name] = Label(label_name)
+            labels[label_name].transitions.append(len(bytecode))
+            bytecode.append(0) # temporarily add 0 instead of label address
         if command in ('PUSH', 'FROMMEM', 'TOMEM', 'AFROMMEM', 'ATOMEM'):
             if not arguments:
                 die(command + ' needs 1 argument', linenum)
-            if arguments[0][0].isnumeric():
-                try:
-                    val = int(arguments[0])
-                except ValueError:
-                    die(command + ' bad command argument', linenum)
-                output += struct.pack(fmt, val)
+            if arguments[0].isnumeric():
+                val = int(arguments[0])
+                bytecode.append(val)
             else:
                 var = arguments[0]
-                if var not in vars_dict:
+                if var not in vars:
                     die('undefined global variable ' + var, linenum)
-                vars_dict[var].links.append(len(output))
-                output += struct.pack(fmt, 0) # temporarily add 0
+                vars[var].links.append(len(bytecode))
+                bytecode.append(0) # temporarily add 0 instead of variable address
         if command == 'DUP':
             if not arguments:
-                output += struct.pack(fmt, 1)
+                bytecode.append(1)
             else:
                 try:
-                    output += struct.pack(fmt, int(arguments[0]))
+                    bytecode.append(int(arguments[0]))
                 except ValueError:
                     die(command + ' bad command argument', linenum)
     # explicitly quit when program ends
-    output += struct.pack(fmt, opcodes.index('QUIT'))
+    bytecode.append(opcodes.index('QUIT'))
 if labels:
     print('labels:')
     for x in labels:
         print('\t', x)
-if vars_dict:
+if vars:
     print('variables:')
-    for x in vars_dict:
-        print('\t', x, '\t', vars_dict[x].size)
+    for x in vars:
+        print('\t', x, '\t', vars[x].size)
 # now we know all labels addresses and
 # can write correct address to every j* and call command
-for label in labels:
-    if labels[label].address == -1:
-        die('needs ' + label + ' label')
-    for transition in labels[label].transitions:
-        output[transition : transition + command_length] = \
-                          struct.pack(fmt, labels[label].address)
+for label in labels.values():
+    label.set_transitions(bytecode)
 # now we can allocate space for all global variables and arrays.
 # this space will be allocated in bytecode file, after the code.
 # and we can write correct variable address in all places where
 # these variable requires
-for var in vars_dict:
-    for link in vars_dict[var].links:
-        output[link : link + command_length] = \
-                    struct.pack(fmt, len(output) // command_length)
-    for x in range(vars_dict[var].size):
-        output += struct.pack(fmt, 0)
-        
-out_file = sys.argv[1] + '.zl'
+for var in vars.values():
+    var.set_links(bytecode)
+    var.allocate_space(bytecode)
 
-with open(out_file, 'bw') as output_file:
-    output_file.write(output)
-    
-print('Bytecode size:', len(output))
+filename = bytecode.save(source)
 print('Launching...\n')
 
-import platform
 if platform.system() == 'Linux':
 	command_to_launch = os.path.join(os.path.abspath(os.path.dirname( \
                      sys.argv[0])), 'stack_machine')
-	os.system(command_to_launch + ' ' + out_file + '')
+	os.system(command_to_launch + ' ' + filename + '')
 else: # windows
 	command_to_launch = os.path.join(os.path.abspath(os.path.dirname( \
                      sys.argv[0])), 'stack_machine.exe')
-	os.system('""' + command_to_launch + '" "' + out_file + '""')
+	os.system('""' + command_to_launch + '" "' + filename + '""')
 
